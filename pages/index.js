9bb75035636a82d16bab41d113581432a27594ac
hoboldinho <gabrielcardoso4123@gmail.com>
2023-10-01 18:31:56 -0300

    adicionando frase impactante

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,19 @@
-function Home () {
-    return <h1>Hmmm pãozinho com manteiga!</h1>
+function Home() {
+    let body = <div>
+        <h1>Conselhos do grande pensador Cesinha...</h1>
+        <p>Nome?</p>
+        <input type="text"></input>
+        <p>Idade?</p>
+        <input type="number"></input>
+        <p>Sim ou Não?</p>
+        <input name="SimOuNao" id="SimOuNao" list="SimOuNaos"></input>
+        <datalist id="SimOuNaos">
+            <option>Sim</option>
+            <option>Não</option>
+        </datalist>
+    </div>
+    console.log(body)
+    return body
 }
 
 export default Home
