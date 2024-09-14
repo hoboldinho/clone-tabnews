6649531a7c6a79e1b28bd15696991bbc9be25734
hoboldinho <gabrielcardoso4123@gmail.com>
2024-09-14 18:57:04 -0300

    Mudando a frase de impacto para uma que realmente faz sentido

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,17 @@
 function Home() {
     let body = <div>
-        <h1>Conselhos do grande pensador Cesinha...</h1>
-        <p>Nome?</p>
-        <input type="text"></input>
-        <p>Idade?</p>
-        <input type="number"></input>
-        <p>Sim ou Não?</p>
-        <input name="SimOuNao" id="SimOuNao" list="SimOuNaos"></input>
-        <datalist id="SimOuNaos">
-            <option>Sim</option>
-            <option>Não</option>
-        </datalist>
+        <h1>FELIZZZZZ Níveeerrr GABIII!!! 🥳🥳🥳</h1>
+        <p>
+            Desejo tudo de bom e melhor sempreeee!<br />
+            Vc é sensacional! Maravilhosamente incrível!<br />
+            SOUeternamenteGRATO 🙏 por ter tido a oportunidade de um dia ter te conhecido<br />
+            Dona dotada dos melhores designs arquitetônicos, das melhores imagens já produzidas e renderizadas 🤭😅<br />
+            Sua companhia é como uma brisa suave e reconfortante<br />
+            Sua presença deixa qualquer lugar ilumidado e B R I L H A N T E ✨<br />
+            Aproveita ao máximo seu dia!<br />
+            😘
+        </p>
     </div>
-    console.log(body)
     return body
 }
 
