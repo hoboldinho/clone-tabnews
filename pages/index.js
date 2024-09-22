814deca6ada57ff7f5bf81078d67b7d4f26a6f91
hoboldinho <gabrielcardoso4123@gmail.com>
2024-09-22 19:53:02 -0300

    passando o prettier em tudo

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,32 @@
 function Home() {
-    let body = <div>
-        <h1>FELIZZZZZ Níveeerrr GABIII!!! 🥳🥳🥳</h1>
-        <p>
-            Desejo tudo de bom e melhor sempreeee!<br />
-            Vc é sensacional! Maravilhosamente incrível!<br />
-            SOUeternamenteGRATO 🙏 por ter tido a oportunidade de um dia ter te conhecido<br />
-            Dona dotada dos melhores designs arquitetônicos, das melhores imagens já produzidas e renderizadas 🤭😅<br />
-            Sua companhia é como uma brisa suave e reconfortante<br />
-            Sua presença deixa qualquer lugar ilumidado e B R I L H A N T E ✨<br />
-            Aproveita ao máximo seu dia!<br />
-            😘
-        </p>
+  let body = (
+    <div>
+      <h1>FELIZZZZZ Níveeerrr GABIII!!! 🥳🥳🥳</h1>
+      <p>
+        Desejo tudo de bom e melhor sempreeee!
+        <br />
+        Vc é sensacional! Maravilhosamente incrível!
+        <br />
+        SOUeternamenteGRATO 🙏 por ter tido a oportunidade de um dia ter te
+        conhecido
+        <br />
+        Dona dotada dos melhores designs arquitetônicos, das melhores imagens já
+        produzidas e renderizadas 🤭😅
+        <br />
+        Sua companhia é como uma brisa suave e reconfortante
+        <br />
+        Sua presença deixa qualquer lugar ilumidado e B R I L H A N T E ✨<br />
+        Aproveita ao máximo seu dia!
+        <br />
+        😘
+      </p>
     </div>
-    return body
+  );
+  return body;
 }
 
-export default Home
+function name(params) {
+  const manteiga = "";
+}
+
+export default Home;
