9e894382e006de3f7f53789d89e44d1cae83bbeb
hoboldinho <gabrielcardoso4123@gmail.com>
2024-11-15 10:16:43 -0300

    finish '/api/v1/status' endpoint

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,13 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const reponseBody = await response.json();
+
+  const parseUpdatedAt = new Date(reponseBody.updated_at).toISOString();
+  expect(reponseBody.updated_at).toEqual(parseUpdatedAt);
+
+  expect(reponseBody.dependencies.database.version).toEqual("16.0");
+  expect(reponseBody.dependencies.database.max_connections).toEqual(100);
+  expect(reponseBody.dependencies.database.opened_connections).toEqual(1);
 });
